30aed5994b123327aaffc74a2459c46e1cabe060
kevingo <kevingo75@gmail.com>
2016-05-15 23:45:31 +0800

    update index

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
-var https = require('https')
-var fs = require('fs')
 var app = express()
 
-var options = {
-  key: fs.readFileSync('./privatekey.pem'),
-  cert: fs.readFileSync('./certificate.pem')
-};
-
 app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
 app.use(bodyParser.json())
 
-// Index route
 app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
-// for Facebook verification
+// Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
@@ -31,9 +19,48 @@ app.get('/webhook/', function (req, res) {
     res.send('Error, wrong token')
 })
 
-// Spin up the server
+app.post('/webhook/', function (req, res) {
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+        if (event.message && event.message.text) {
+            text = event.message.text
+            var space = text.indexOf(' ');
+            var name = text.substring(0, space);
+            var msg = '';
+            if (name === '你') {
+            	msg = '你比較苦...';
+            } else {
+            	msg = name + ' 心裡苦，但是 ' + name + ' 不說';
+            }
+            sendTextMessage(sender, msg)
+        }
+    }
+    res.sendStatus(200)
+})
+
+function sendTextMessage(sender, text) {
+    messageData = {
+        text:text
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
-
-https.createServer(options, app).listen(process.env.SSLPORT || 5001);
